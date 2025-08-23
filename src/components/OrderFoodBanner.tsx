6b59fc222bbf47601afe8dc9c408e181910da49c
Haider <haiderraza7516@gmail.com>
2025-08-24 00:51:12 +0500

    A change

--- src/components/OrderFoodBanner.tsx
+++ src/components/OrderFoodBanner.tsx
@@ -8,9 +8,9 @@ import AddressDetail from "./AddressDetail";
 
 export default function OrderFoodBanner({foodData}:OrderFoodBannerProps){
   return (
-    <div>
+    <div className="mr-10 sm:mr-0">
         <FoodOrderBanner/>
-      <div className="lg:grid lg:grid-cols-3 flex flex-wrap gap-10 lg:gap-0 sm:gap-6">
+      <div className="lg:grid lg:grid-cols-3 flex flex-wrap gap-10 lg:gap-0 sm:gap-6 ">
 <div className="col-span-2 ">
   <div className="lg:grid lg:grid-cols-3 flex flex-wrap sm:gap-2 sm:justify-around 
    gap-4 sm:p-10 p-5 sm:items-center">
@@ -19,9 +19,14 @@ export default function OrderFoodBanner({foodData}:OrderFoodBannerProps){
   ))}
   </div>
 </div>
-     <div className="lg:flex lg:flex-col sm:flex gap3 p-5">
-          <Cart />
-          <div>
+     <div className="lg:flex lg:flex-col sm:flex gap-3 p-5 ">
+           <div className="mr-10 sm:mr-0">
+            <Cart />
+           </div>
+            
+       
+          
+          <div className="mr-10 sm:mr-0">
               <Checkout />
               <AddressDetail/>
             </div>
